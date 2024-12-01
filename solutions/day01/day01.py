20c67d90a63d96ae6d6457f8fbe95d83a32fe797
josephburgess <josephburgess@gmail.com>
2024-12-01 14:36:34 +0000

    update solve function for day1

--- solutions/day01/day01.py
+++ solutions/day01/day01.py
@@ -1,10 +1,14 @@
-from .logic import parse_arrays, calculate_distance
+from .logic import calculate_similarity_score, parse_arrays, calculate_distance
 
 def solve():
     with open("input/day01.txt", "r") as f:
         data = f.read()
 
     left, right = parse_arrays(data)
+
     total_distance = calculate_distance(left, right)
 
-    return f"Day 1: The total distance is {total_distance}"
+    similarity_score = calculate_similarity_score(left, right)
+
+    return (f"Day 1 - total distance: {total_distance}\n"
+        f"Day 1 - similarity score: {similarity_score}")
