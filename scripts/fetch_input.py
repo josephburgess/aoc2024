6c5bb20d2aef43b1a8ee96c64cc0cc879358fc34
josephburgess <josephburgess@gmail.com>
2024-12-02 12:20:12 +0000

    Rename

--- scripts/fetch_input.py
+++ scripts/fetch_input.py
@@ -17,7 +17,7 @@ def fetch_input(day: int):
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         INPUT_DIR.mkdir(parents=True, exist_ok=True)
-        input_file = INPUT_DIR / f"day_{day:02}.txt"
+        input_file = INPUT_DIR / f"day{day:02}.txt"
         with open(input_file, "w") as f:
             _ = f.write(response.text)
         print(f"Input for day {day} saved to {input_file}")
