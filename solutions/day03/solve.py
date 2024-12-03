777f704c9f1c15fa5ee5e21995686906c7519946
josephburgess <josephburgess@gmail.com>
2024-12-03 10:08:58 +0000

    Solve question 1

--- solutions/day03/solve.py
+++ solutions/day03/solve.py
@@ -1,7 +1,18 @@
 import re
 
+from custom_types import Solution
+
 
 def extract_pairs(data: str) -> list[tuple[int, int]]:
     pattern = r"mul\((\d+),\s*(\d+)\)"
     matches: list[tuple[str, str]] = re.findall(pattern, data)
     return [(int(x), int(y)) for x, y in matches]
+
+
+def multiply_and_sum(pairs: list[tuple[int, int]]):
+    return sum(x * y for x, y in pairs)
+
+
+def solve(data: str) -> Solution:
+    solution_one = multiply_and_sum(extract_pairs(data))
+    return solution_one, 0
--- tests/test_day03.py
+++ tests/test_day03.py
@@ -1,5 +1,5 @@
 
-from solutions.day03.solve import extract_pairs
+from solutions.day03.solve import extract_pairs, multiply_and_sum
 
 
 class TestExtractPairs:
@@ -9,3 +9,13 @@ class TestExtractPairs:
 
     def test_extracts_two_pairs(self):
         assert extract_pairs('mul(1,1)mul(2,2)') == [(1, 1), (2, 2)]
+
+    def test_puzzle_example(self):
+        example = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+        assert extract_pairs(example) == [(2, 4), (5, 5), (11, 8), (8, 5)]
+
+
+class TestMultiplyAndSum:
+    def test_puzzle_example(self):
+        example = [(2, 4), (5, 5), (11, 8), (8, 5)]
+        assert multiply_and_sum(example) == 161
