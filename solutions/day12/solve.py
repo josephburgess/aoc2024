b4b087c98e135bc78332a976eb5c2c844638687a
josephburgess <josephburgess@gmail.com>
2024-12-13 17:52:35 +0000

    Day12: Solve part 2

--- solutions/day12/solve.py
+++ solutions/day12/solve.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 from custom_types import Grid, Location, Pair
 from utils import parse_grid
 
@@ -19,48 +17,98 @@ def is_valid_plot(r: int, c: int, grid: Grid, visited: set[Location], plot_type:
     )
 
 
-def flood_fill(r: int, c: int, plot_type: str, visited: set[Location], grid: Grid) -> tuple[int, int]:
-    if not is_valid_plot(r, c, grid, visited, plot_type):
-        return 0, 0
-
+def flood_fill(r: int, c: int, plot_type: str, visited: set[Location], grid: Grid) -> tuple[int, int, set[Location]]:
+    stack = [(r, c)]
     visited.add((r, c))
-
-    area = 1
+    region_cells = set([(r, c)])
+    area = 0
     perimeter = 0
 
-    for dr, dc in DIRECTIONS:
-        nr, nc = r + dr, c + dc
-        if in_bounds(nr, nc, grid):
-            if grid[nr][nc] != plot_type:
+    while stack:
+        cr, cc = stack.pop()
+        area += 1
+
+        for dr, dc in DIRECTIONS:
+            nr, nc = cr + dr, cc + dc
+            if in_bounds(nr, nc, grid):
+                if grid[nr][nc] != plot_type:
+                    perimeter += 1
+                else:
+                    if (nr, nc) not in visited:
+                        visited.add((nr, nc))
+                        stack.append((nr, nc))
+                        region_cells.add((nr, nc))
+            else:
                 perimeter += 1
-            elif (nr, nc) not in visited:
-                sub_area, sub_perimeter = flood_fill(nr, nc, plot_type, visited, grid)
-                area += sub_area
-                perimeter += sub_perimeter
-        else:
-            perimeter += 1
 
-    return area, perimeter
+    return area, perimeter, region_cells
 
 
-def calculate_fence_price(grid: Grid):
+def count_sides(region_cells: set[Location]) -> int:
+    s = region_cells
+    sides = 0
+
+    def in_region(y, x):
+        return (y, x) in s
+
+    for (y, x) in region_cells:
+        north = (y - 1, x)
+        east = (y, x + 1)
+        south = (y + 1, x)
+        west = (y, x - 1)
+        northeast = (y - 1, x + 1)
+        southeast = (y + 1, x + 1)
+        southwest = (y + 1, x - 1)
+        northwest = (y - 1, x - 1)
+
+        # outside curve
+        if (not in_region(*north)) and (not in_region(*east)):
+            sides += 1
+        if (not in_region(*east)) and (not in_region(*south)):
+            sides += 1
+        if (not in_region(*south)) and (not in_region(*west)):
+            sides += 1
+        if (not in_region(*west)) and (not in_region(*north)):
+            sides += 1
+
+        # inside curve
+        if (not in_region(*northwest)) and in_region(*north) and in_region(*west):
+            sides += 1
+        if (not in_region(*northeast)) and in_region(*north) and in_region(*east):
+            sides += 1
+        if (not in_region(*southeast)) and in_region(*south) and in_region(*east):
+            sides += 1
+        if (not in_region(*southwest)) and in_region(*south) and in_region(*west):
+            sides += 1
+
+    return sides
+
+
+def calculate_prices(grid: Grid) -> tuple[int, int]:
     visited: set[Location] = set()
-    total_price = 0
+    total_part_one = 0
+    total_part_two = 0
 
     for r in range(len(grid)):
         for c in range(len(grid[0])):
             if (r, c) not in visited:
                 plot_type = grid[r][c]
-                area, perimeter = flood_fill(r, c, plot_type, visited, grid)
-                price = area * perimeter
-                total_price += price
+                area, perimeter, region_cells = flood_fill(r, c, plot_type, visited, grid)
+                region_sides = count_sides(region_cells)
 
-    return total_price
+                price_one = area * perimeter
+                price_two = area * region_sides
+
+                total_part_one += price_one
+                total_part_two += price_two
+
+    return total_part_one, total_part_two
 
 
 def solve(data: str) -> Pair:
-    price = calculate_fence_price(parse_grid(data))
-    return (price, 1)
+    grid = parse_grid(data)
+    part_one_price, part_two_price = calculate_prices(grid)
+    return (part_one_price, part_two_price)
 
 
 # parse into a grid
--- tests/test_day12.py
+++ tests/test_day12.py
@@ -23,4 +23,4 @@ def example_data():
 
 
 def test_example_data(example_data: str):
-    assert solve(example_data) == (1930, 1)
+    assert solve(example_data) == (1930, 1206)
